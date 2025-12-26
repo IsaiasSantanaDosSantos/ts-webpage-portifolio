6d732c33c9b721bc3effb579b8f61fb1e26735aa
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2025-12-26 15:12:56 -0300

    test: comments content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-// import NavBar from '../components/NavBar/navBarTemp';
+import NavBar from './components/NavBar/navBar';
 
 function App() {
   return (
-    <div>
-      <h1>Teste</h1>
-
-      {/* <NavBar /> */}
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 }
 
