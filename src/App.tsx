e1d74fceb1ada7fdd2554acf180e8b973e4eb345
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2025-12-29 12:44:02 -0300

    feat: create the change logo with theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,81 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { toast, ToastContainer } from 'react-toastify';
+import { ThemeProviderContext, useTheme } from './context/themeContext';
 import { MobileMenuProvider } from './context/mobileMenuContext';
-import { lightTheme, darkTheme, type ThemeType } from './style/theme';
+// import { lightTheme, darkTheme, type ThemeType } from './style/theme';
 
 import NavBar from './components/NavBar/navBar';
 import GlobalStyles from './style/GlobalStyles';
 
-function App() {
-  const [theme, setTheme] = useState<ThemeType>(lightTheme);
+const AppContent = () => {
+  const { themeObject } = useTheme();
   const { t } = useTranslation();
 
   useEffect(() => {
     toast.success(t('toast_info_message'));
-    const savedTheme = localStorage.getItem('appTheme');
-    if (savedTheme === 'dark') {
-      setTheme(darkTheme);
-    } else if (savedTheme === 'light') {
-      setTheme(lightTheme);
-    } else {
-      const prefersDark = window.matchMedia(
-        '(prefers-color-scheme: dark)'
-      ).matches;
-      setTheme(prefersDark ? darkTheme : lightTheme);
-    }
   }, []);
 
-  const toggleTheme = () => {
-    const newTheme = theme === lightTheme ? darkTheme : lightTheme;
-    setTheme(newTheme);
-    localStorage.setItem('appTheme', newTheme === darkTheme ? 'dark' : 'light');
-  };
-
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themeObject}>
       <GlobalStyles />
       <ToastContainer theme="colored" />
       <MobileMenuProvider>
-        <NavBar
-          toggleTheme={toggleTheme}
-          currentTheme={theme === lightTheme ? 'light' : 'dark'}
-        />
+        <NavBar />
       </MobileMenuProvider>
-      <ToastContainer />
     </ThemeProvider>
   );
+};
+
+function App() {
+  return (
+    <ThemeProviderContext>
+      <AppContent />
+    </ThemeProviderContext>
+  );
 }
+
+// function App() {
+//   const [theme, setTheme] = useState<ThemeType>(lightTheme);
+//   const { t } = useTranslation();
+
+//   useEffect(() => {
+//     toast.success(t('toast_info_message'));
+//     const savedTheme = localStorage.getItem('appTheme');
+//     if (savedTheme === 'dark') {
+//       setTheme(darkTheme);
+//     } else if (savedTheme === 'light') {
+//       setTheme(lightTheme);
+//     } else {
+//       const prefersDark = window.matchMedia(
+//         '(prefers-color-scheme: dark)'
+//       ).matches;
+//       setTheme(prefersDark ? darkTheme : lightTheme);
+//     }
+//   }, []);
+
+//   const toggleTheme = () => {
+//     const newTheme = theme === lightTheme ? darkTheme : lightTheme;
+//     setTheme(newTheme);
+//     localStorage.setItem('appTheme', newTheme === darkTheme ? 'dark' : 'light');
+//   };
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <GlobalStyles />
+//       <ToastContainer theme="colored" />
+//       <ThemeContext>
+//         <MobileMenuProvider>
+//           <NavBar
+//             toggleTheme={toggleTheme}
+//             currentTheme={theme === lightTheme ? 'light' : 'dark'}
+//           />
+//         </MobileMenuProvider>
+//       </ThemeContext>
+//       <ToastContainer />
+//     </ThemeProvider>
+//   );
+// }
 
 export default App;
--- src/components/Logo/logo.tsx
+++ src/components/Logo/logo.tsx
@@ -1,30 +1,30 @@
-import { Link } from 'react-scroll';
+import { useTheme } from '../../context/themeContext';
 
-import logoVideo from '../../assets/video/animateSantanaWebDeveloper.mp4';
+import logoLightVideo from '../../assets/video/animateSantanaWebDeveloper.mp4';
+import logoDarkVideo from '../../assets/video/blackWebDeveloperLogoAnimation.mp4';
 import logoImg from '../../../public/img/logo-512.png';
 
 import * as S from './logo.styled';
+import { Link } from 'react-scroll';
 
 export const Logo = () => {
+  // const { theme } = useTheme();
+  const { themeMode } = useTheme();
+
+  const videoSrc = themeMode === 'dark' ? logoDarkVideo : logoLightVideo;
+
   const isVideoSupported = () => {
     const videoElement = document.createElement('video');
     return Boolean(videoElement.canPlayType);
   };
 
   return (
-    <Link
-      activeClass="actived"
-      to="home"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={800}
-    >
+    <Link to="home" spy={true} smooth={true} offset={-70} duration={800}>
       <S.LogoBox>
         {isVideoSupported() ? (
-          <video autoPlay loop muted>
-            <source src={logoVideo} type="video/mp4" />
-            Your browser does not support video in HTML 5
+          <video autoPlay loop muted key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+            Your browser does not support video in HTML5
           </video>
         ) : (
           <img src={logoImg} alt="Santana's logo" />
@@ -33,3 +33,39 @@ export const Logo = () => {
     </Link>
   );
 };
+
+// import { Link } from 'react-scroll';
+
+// import logoVideo from '../../assets/video/animateSantanaWebDeveloper.mp4';
+// import logoImg from '../../../public/img/logo-512.png';
+
+// import * as S from './logo.styled';
+
+// export const Logo = () => {
+//   const isVideoSupported = () => {
+//     const videoElement = document.createElement('video');
+//     return Boolean(videoElement.canPlayType);
+//   };
+
+//   return (
+//     <Link
+//       activeClass="actived"
+//       to="home"
+//       spy={true}
+//       smooth={true}
+//       offset={-70}
+//       duration={800}
+//     >
+//       <S.LogoBox>
+//         {isVideoSupported() ? (
+//           <video autoPlay loop muted>
+//             <source src={logoVideo} type="video/mp4" />
+//             Your browser does not support video in HTML 5
+//           </video>
+//         ) : (
+//           <img src={logoImg} alt="Santana's logo" />
+//         )}
+//       </S.LogoBox>
+//     </Link>
+//   );
+// };
