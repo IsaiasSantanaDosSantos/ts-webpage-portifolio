6276c1e963a44467e7629ac6012aab99dd27d6ff
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2025-12-29 12:48:29 -0300

    docs: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import { toast, ToastContainer } from 'react-toastify';
 import { ThemeProviderContext, useTheme } from './context/themeContext';
 import { MobileMenuProvider } from './context/mobileMenuContext';
-// import { lightTheme, darkTheme, type ThemeType } from './style/theme';
 
 import NavBar from './components/NavBar/navBar';
 import GlobalStyles from './style/GlobalStyles';
@@ -35,47 +34,5 @@ function App() {
     </ThemeProviderContext>
   );
 }
-
-// function App() {
-//   const [theme, setTheme] = useState<ThemeType>(lightTheme);
-//   const { t } = useTranslation();
-
-//   useEffect(() => {
-//     toast.success(t('toast_info_message'));
-//     const savedTheme = localStorage.getItem('appTheme');
-//     if (savedTheme === 'dark') {
-//       setTheme(darkTheme);
-//     } else if (savedTheme === 'light') {
-//       setTheme(lightTheme);
-//     } else {
-//       const prefersDark = window.matchMedia(
-//         '(prefers-color-scheme: dark)'
-//       ).matches;
-//       setTheme(prefersDark ? darkTheme : lightTheme);
-//     }
-//   }, []);
-
-//   const toggleTheme = () => {
-//     const newTheme = theme === lightTheme ? darkTheme : lightTheme;
-//     setTheme(newTheme);
-//     localStorage.setItem('appTheme', newTheme === darkTheme ? 'dark' : 'light');
-//   };
-
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyles />
-//       <ToastContainer theme="colored" />
-//       <ThemeContext>
-//         <MobileMenuProvider>
-//           <NavBar
-//             toggleTheme={toggleTheme}
-//             currentTheme={theme === lightTheme ? 'light' : 'dark'}
-//           />
-//         </MobileMenuProvider>
-//       </ThemeContext>
-//       <ToastContainer />
-//     </ThemeProvider>
-//   );
-// }
 
 export default App;
